c7a7c9a37c7f6f4b71dea143fc75c1644c0e8b1d
JPisaBrony <jdpohl789@gmail.com>
2016-03-08 12:43:28 -0600

    + ability to all images

--- discord-downloader.py
+++ discord-downloader.py
@@ -16,15 +16,30 @@ if not os.path.exists("images"):
 r = requests.post("https://discordapp.com/api/auth/login", data = {"email":email, "password":password})
 token = r.json()["token"]
 headers = {"authorization": token}
-messages = requests.get("https://discordapp.com/api/channels/" + str(channel) + "/messages?limit=" + str(100), headers=headers)
+messages = requests.get("https://discordapp.com/api/channels/" + str(channel) + "/messages", headers=headers)
 msg_json = messages.json()
-for x in msg_json:
-	for y in x["attachments"]:
-		url = y["url"]
-		img = requests.get(url, stream=True)
-		if img.status_code == 200:
-			name = url.split("/")
-			with open(str("images/") + name[6], "wb+") as f:
-				for chunk in img:
-					f.write(chunk)
-		print url
+
+processed_urls = 0
+finished = True
+
+while finished:
+	for x in msg_json:
+		for y in x["attachments"]:
+			url = y["url"]
+			img = requests.get(url, stream=True)
+			img_name = ""
+			if img.status_code == 200:
+				name = url.split("/")
+				img_name = name[6]
+				with open(str("images/") + name[6], "wb+") as f:
+					for chunk in img:
+						f.write(chunk)
+			print img_name
+		processed_urls += 1
+	if processed_urls == 50:
+		last_msg = msg_json[49]["id"]
+		messages = requests.get("https://discordapp.com/api/channels/" + str(channel) + "/messages?before=" + str(last_msg), headers=headers)
+		msg_json = messages.json()
+		processed_urls = 0
+	else:
+		finished = False
