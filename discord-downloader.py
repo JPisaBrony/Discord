a188ca685ec9788ec35da2c199e010f532b396c7
JPisaBrony <jdpohl789@gmail.com>
2016-03-19 01:33:44 -0500

    + multithreaded download capabilities

--- discord-downloader.py
+++ discord-downloader.py
@@ -1,3 +1,4 @@
+from multiprocessing import Process
 import requests
 import os
 import sys
@@ -22,20 +23,30 @@ msg_json = messages.json()
 processed_urls = 0
 finished = True
 
+def fileDownload(url, dir):
+        img = requests.get(url, stream=True)
+        img_name = ""
+        if img.status_code == 200:
+		name = url.split("/")
+		img_name = name[6]
+		with open(str(dir) + name[6], "wb+") as f:
+			for chunk in img:
+				f.write(chunk)
+	print img_name
+
 while finished:
+        jobs = []
 	for x in msg_json:
 		for y in x["attachments"]:
 			url = y["url"]
-			img = requests.get(url, stream=True)
-			img_name = ""
-			if img.status_code == 200:
-				name = url.split("/")
-				img_name = name[6]
-				with open(str("images/") + name[6], "wb+") as f:
-					for chunk in img:
-						f.write(chunk)
-			print img_name
+                        p = Process(target=fileDownload, args=(url, "images/"))
+                        p.start()
+                        jobs.append(p)
 		processed_urls += 1
+        
+        for x in jobs:
+            p.join()
+        
 	if processed_urls == 50:
 		last_msg = msg_json[49]["id"]
 		messages = requests.get("https://discordapp.com/api/channels/" + str(channel) + "/messages?before=" + str(last_msg), headers=headers)
