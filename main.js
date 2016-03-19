2ce9ff22563e745eb836b8ad1e14d265faf11158
JPisaBrony <jdpohl789@gmail.com>
2016-03-19 14:56:42 -0500

    + code cleanup and started working on seperate pages

---
+++ main.js
@@ -0,0 +1,24 @@
+function displayImages(dir, thumb_dir) {
+        $.ajax({
+            url: thumb_dir,
+            success: function (data) {
+                $(data).find("a").attr("href", function (i, val) {
+                    if( val.match(/\.(jpe?g|png|gif)$/) ) {
+                        $("body").append( "<img src='" + thumb_dir + "/" + val +"'>" );
+                    }
+                });
+                $("img").click(function(){
+                    var url = this.src.replace(thumb_dir, dir);
+                    var type = url.split(".").pop();
+                    var name = url.split("/")[4];
+                    var x = new XMLHttpRequest();
+                    x.open("GET", url, true);
+                    x.responseType = "blob";
+                    x.onload = function(e) {
+                        download(x.response, name, dir + "/" + type);
+                    }
+                    x.send();
+                });
+            }
+        }); 
+}
